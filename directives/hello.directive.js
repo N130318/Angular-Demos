6e31b7678e6f491190ae063346306b17ef8c7807
namitamalik <namita1990@gmail.com>
2017-06-28 15:25:46 +0530

    Adding link function

--- directives/hello.directive.js
+++ directives/hello.directive.js
@@ -1,14 +1,23 @@
-(function(){
-		var directiveDemoApp = angular.module('directiveDemoApp');
+(function() {
+  var directiveDemoApp = angular.module('directiveDemoApp');
 
-	directiveDemoApp.directive('helloUser',function(){
-		return {
-			template:'<span ng-click="showDetails()">Hello {{name}}</span>',
-			controller: function($scope){
-				$scope.showDetails = function(){
-					alert("Student-AU 2017");
-				}
-			}
-		}
-	})
+  directiveDemoApp.directive('helloUser', function() {
+    return {
+      template: '<span ng-click="showDetails()">Hello {{name}}</span>',
+      controller: function($scope) {
+        $scope.showDetails = function() {
+          alert("Student-AU 2017");
+        }
+      },
+      link: function(scope, elem, attrs) {
+        console.log(scope);
+        console.log(elem);
+        console.log(attrs);
+        scope.$watch('name',function(oldVal,newVal){
+        	if(oldVal !== newVal)
+        	elem[0].style.color = "blue";
+        })
+      }
+    }
+  })
 })()
