f3e6e40268ff3296de6b8dc3ea87875d0734f301
namitamalik <namita1990@gmail.com>
2017-07-05 16:55:43 +0530

    Adding filter demo

---
+++ filter/filter-app.controller.js
@@ -0,0 +1,14 @@
+(function() {
+  var filterDemoApp = angular.module('filterDemoApp');
+  filterDemoApp.controller('FilterDemoAppController', ['$scope', '$filter', '$http', function($scope, $filter, $http) {
+    $scope.data = {};
+    $http.get('./employees.json').then(function(response) {
+      $scope.data.employeeList = response.data.map(function(obj) {
+        obj.salary = $filter('currency')(obj.salary, obj.currencyCode);
+        return obj;
+      });
+    }, function(error) {
+      alert('An error occurred!')
+    })
+  }])
+})()
