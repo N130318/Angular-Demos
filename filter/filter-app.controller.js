9905fb0b2843bc29643bb9337293bfbda3e18f3d
namitamalik <namita1990@gmail.com>
2017-07-26 00:56:30 +0530

    fixes

--- filter/filter-app.controller.js
+++ filter/filter-app.controller.js
@@ -2,6 +2,7 @@
   var filterDemoApp = angular.module('filterDemoApp');
   filterDemoApp.controller('FilterDemoAppController', ['$scope', '$filter', '$http', function($scope, $filter, $http) {
     $scope.data = {};
+    $scope.date = new Date();
     $http.get('./employees.json').then(function(response) {
       $scope.data.employeeList = response.data.map(function(obj) {
         obj.salary = $filter('currency')(obj.salary, obj.currencyCode);
